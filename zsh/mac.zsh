6a1a6f75555a2e5d89682cb949c655ca4f8e36c5
Ben Hayden <ben@britecore.com>
2014-09-08 17:01:48 -0500

    Better Mac zsh

--- zsh/mac.zsh
+++ zsh/mac.zsh
@@ -1,12 +1,24 @@
-ZSH_DIR=$(dirname `readlink $HOME/.zshrc`)
-export PATH="$HOME/bin:$HOME/Documents/go-zone/bin:$(brew --prefix coreutils)/libexec/gnubin:$(brew --prefix findutils)/bin:$(brew --prefix ruby)/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:${PATH}"
-export GOPATH="$HOME/Documents/go-zone"
+ZSHRC=$(readlink "$HOME"/.zshrc)
+ZSH_DIR=$(dirname "$ZSHRC")
 
+export PATH="$HOME/bin:$HOME/Documents/go-zone/bin:$(brew --prefix go)/libexec/bin:$(brew --prefix gnu-sed)/libexec/gnubin:$(brew --prefix coreutils)/libexec/gnubin:$(brew --prefix findutils)/bin:$(brew --prefix ruby)/bin:/usr/local/share/npm/bin:/usr/local/sbin:/usr/local/bin:${PATH}"
+export MANPATH="$(brew --prefix gnu-sed)/libexec/gnuman:$MANPATH"
+export GOPATH="$HOME/Documents/go-zone"
+export NODE_PATH="/usr/local/lib/node_modules"
+
+# Homebrew cask aliases
+alias cask_install="brew cask install --appdir=/Applications"
+alias cask_uninstall="brew cask uninstall"
+alias cask_search="brew cask search"
+alias cask_list="brew cask list"
+# Set up haste to use our BriteCore server
 alias work_haste='HASTE_SERVER=http://hastebin.britecorepro.com haste'
-alias vud='vagrant up dev'
-alias vhd='vagrant halt dev'
-alias vsd='vagrant ssh dev'
-alias vpd='vagrant provision dev'
+# Vagrant Aliases
+alias vud='cd ~/BC-Up && vagrant up dev'
+alias vhd='cd ~/BC-Up && vagrant halt dev'
+alias vsd='cd ~/BC-Up && vagrant ssh dev'
+alias vpd='cd ~/BC-Up && vagrant provision dev'
+alias vagrantrun="ssh -i ~/.vagrant.d/insecure_private_key -c none -YC -p2222 vagrant@localhost"
 # Better ls alias for Mac
 alias ls='ls -GpFh'
 
