070fa40b7dbe38e07431f09bf7db3bad6b47199d
Jacob Foster <jacob@britecore.com>
2014-11-14 16:08:11 -0600

    helpful alias for dev machine when running reports

--- zsh/linux.zsh
+++ zsh/linux.zsh
@@ -4,6 +4,7 @@ export GOPATH="$HOME/Documents/go-zone"
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
 
 alias ls='ls --color=auto'
+alias hammertime='sudo supervisorctl stop all && sudo service rabbitmq-server stop && sudo service mysql restart'
 
 source "$ZSH_DIR/common.zsh"
 
